f5bbf575214f840c253e05a8e8a977243f16523c
Taylor Aucoin <colorwheeloflife@gmail.com>
2016-10-21 01:43:34 +0000

    Have login and setup pages displaying. Configuring database now.

--- db/migrate/20161020200832_create_owners.rb
+++ db/migrate/20161020200832_create_owners.rb
@@ -2,7 +2,6 @@ class CreateOwners < ActiveRecord::Migration[5.0]
   def change
     create_table :owners do |t|
       t.string :name
-      t.string :email
       t.integer :league_id
 
       t.timestamps
