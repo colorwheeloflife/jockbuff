120d9415c94b0418ad1ca1bdb8274ef75903493f
Sarah Veness <sarahhveness@gmail.com>
2016-10-21 02:47:14 +0000

    oauth fix, users can now sign in with their yahoo accounts

--- Gemfile
+++ Gemfile
@@ -16,16 +16,18 @@ group :development, :test do
   gem 'byebug', platform: :mri
 end
 group :development do
-  gem 'web-console'
+  # gem 'web-console'
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap-sass'
 gem 'high_voltage'
 gem 'omniauth'
-gem 'omniauth-twitter'
+gem 'omniauth-yahoo-oauth2', git: 'https://github.com/sarahveness/omniauth-yahoo-oauth2.git'
+# gem 'httplog'
 group :development do
   gem 'better_errors'
   gem 'rails_layout'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
 
   def new
-    redirect_to '/auth/twitter'
+    redirect_to '/auth/yahoo'
   end
 
   def create
     auth = request.env["omniauth.auth"]
+    puts "\n\n\n\nAUTH: #{auth}\n\n\n"
     user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
@@ -21,5 +22,6 @@ class SessionsController < ApplicationController
   def failure
     redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
   end
+
 
 end
