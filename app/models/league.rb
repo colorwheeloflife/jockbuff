255b1d0e16b13b0599f6f2d35c58cd6f9933a28e
Sarah Veness <sarahhveness@gmail.com>
2016-10-22 21:25:17 +0000

    updated user model, taking out comments / league model the same

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,2 +1,4 @@
 class League < ApplicationRecord
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,3 @@
-# class User < ActiveRecord::Base
-#   has_secure_password
-#   has_many :reviews
-
-#   before_save { self.email = email.downcase.strip }
-
-
-# end
-
 class User < ApplicationRecord
 
   def self.create_with_omniauth(auth)
