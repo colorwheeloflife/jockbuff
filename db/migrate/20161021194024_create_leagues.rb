7ce3a81edcff8c2e532fc1385cd4e450d916a018
Mitchell ONeillgit config --global user.name Mitchell <7moneill@gmail.com>
2016-10-24 19:57:03 +0000

    updated leagues migration

--- db/migrate/20161021194024_create_leagues.rb
+++ db/migrate/20161021194024_create_leagues.rb
@@ -1,9 +1,13 @@
 class CreateLeagues < ActiveRecord::Migration[5.0]
   def change
+
     create_table :leagues do |t|
       t.string :name
+      t.belongs_to :user_id, index: true
       t.string :sport
-
+      t.string :user_team
+      t.string :user_logo
+      t.string :league_name
       t.timestamps
     end
   end
