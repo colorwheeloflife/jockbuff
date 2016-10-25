80252e498fb381d572bdbfb7f926bb2073e69644
Mitchell ONeillgit config --global user.name Mitchell <7moneill@gmail.com>
2016-10-25 19:54:19 +0000

    controller changes for owners and players

--- app/controllers/player_passport_controller.rb
+++ app/controllers/player_passport_controller.rb
@@ -2,23 +2,19 @@ class PlayerPassportController < ApplicationController
 
   def create (leauge_id)
     @team_info_arr = league_call(league_id)
+    @current_user = current_user
     @team_info_arr.map do |team|
-      player_info = owner_info["roster"]["players"]["player"]
+      player_info = team["roster"]["players"]["player"]
+      team_key = team["team_key"]
       entry = player_info.map do |t|
-      entry = {
-      player_id: t["player_id"],
-      current_position: t["selected_position"]["position"]
-      }
-          league_full = HTTParty.get("#{@yahoo_root}league/#{league_key}/settings", headers:{
-      "Authorization" => "Bearer #{token}"
-      })
-      num_teams = league_full["fantasy_content"]["league"]["num_teams"]
-      team_arr = (1..num_teams).to_a
-      team_arr.each do |team|
-        owner_info = HTTParty.get("#{@yahoo_root}team/#{league_key}.t.#{team}/roster/players", headers:{
-        "Authorization" => "Bearer #{token}"
-        })
-      end
-    end
+        entry = {
+        player_id: t["player_id"],
+        position: t["selected_position"]["position"]
+        team_key: team_key
+        }
+        @player = PlayerPassport.create(entry)
+        end
+     end
   end
+end
 
