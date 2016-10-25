d3385e35d5db376bc98ef8dc892a9fb24e63f32d
Sarah Veness <sarahhveness@gmail.com>
2016-10-25 01:59:07 +0000

    paths updated, no longer hardcoded

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -36,6 +36,12 @@ end
       @league = League.create(league_info)
       end
     end
-    redirect_to "/users/#{@current_user.id}/leagues"
+    redirect_to user_leagues_path(current_user)
   end
+
+  def show
+    @current_user = current_user
+  end
+
+
 end
