3d3ca3d79a7af10d3c51c5714c0e8bdcbe224b98
Mitchell ONeillgit config --global user.name Mitchell <7moneill@gmail.com>
2016-10-25 19:59:42 +0000

    added routes to test controllers

--- db/migrate/20161025012955_create_player_passports.rb
+++ db/migrate/20161025012955_create_player_passports.rb
@@ -2,7 +2,7 @@ class CreatePlayerPassports < ActiveRecord::Migration[5.0]
   def change
     create_table :player_passports do |t|
       t.integer :owner_id
-      t.string :player_id
+      t.string :player_key
       t.string :position
       t.timestamps
     end
