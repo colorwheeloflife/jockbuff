73ee04e28cc1d152b36643e55ef88620cf6a8c46
Taylor Aucoin <colorwheeloflife@gmail.com>
2016-10-25 21:34:26 +0000

    Using waypoint, have first graph applying function, but only with a delay, while the other graphs on the page do not display the functionality at all. No errors in console.

--- app/assets/javascripts/aline-graph.js
+++ app/assets/javascripts/aline-graph.js
@@ -1,48 +1,51 @@
-$(function () {
-    $('#line_graph').highcharts({
-        title: {
-            text: '2016 Best Players',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: Yahoo Fantasy Sports',
-            x: -20
-        },
-        xAxis: {
-            categories: ['G', 'A', 'P', '+/-', 'PPG', 'PPA',
-                'PPP', 'SHG', 'SHA', 'SHP', 'GWG', 'GTG','SOG', 'SH%', 'FW', 'FL', 'HIT', 'BLK', 'TOTAL']
-        },
-        yAxis: {
+$(function() {
+    $('#line_graph').waypoint(function () {
+        $('#line_graph').highcharts({
             title: {
-                text: 'Season Total'
+                text: '2016 Best Players',
+                x: -20 //center
             },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
+            subtitle: {
+                text: 'Source: Yahoo Fantasy Sports',
+                x: -20
+            },
+            xAxis: {
+                categories: ['G', 'A', 'P', '+/-', 'PPG', 'PPA',
+                    'PPP', 'SHG', 'SHA', 'SHP', 'GWG', 'GTG','SOG', 'SH%', 'FW', 'FL', 'HIT', 'BLK', 'TOTAL']
+            },
+            yAxis: {
+                title: {
+                    text: 'Season Total'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'P.Kane',
+                data: [82, 46, 60, 106, 17, 30, 37, 21, 11, 40, .216, 17, 20, 0, 0, 9, 287, .160]
+            },
+            {
+                name: 'S.Crosby',
+                data: [80, 36, 49, 85, 19, 42, 90, 35, 986, 921, .517, 10, 14, 0, 0, 9, 248, .145]
+            },
+            {
+                name: 'A.Ovenchkin',
+                data: [79, 50, 21, 71, 21, 53, 225, 34, 0, 0, .000, 19, 5, 0, 0, 8, 398, .126]
+            },
+            {
+                name: 'D. Sedin',
+                data: [82, 28, 33, 61, 7, 36, 20, 19, 5, 14, .263, 8, 11, 0, 1, 6, 258, .109]
             }]
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'P.Kane',
-            data: [82, 46, 60, 106, 17, 30, 37, 21, 11, 40, .216, 17, 20, 0, 0, 9, 287, .160]
-        },
-        {
-            name: 'S.Crosby',
-            data: [80, 36, 49, 85, 19, 42, 90, 35, 986, 921, .517, 10, 14, 0, 0, 9, 248, .145]
-        },
-        {
-            name: 'A.Ovenchkin',
-            data: [79, 50, 21, 71, 21, 53, 225, 34, 0, 0, .000, 19, 5, 0, 0, 8, 398, .126]
-        },
-        {
-            name: 'D. Sedin',
-            data: [82, 28, 33, 61, 7, 36, 20, 19, 5, 14, .263, 8, 11, 0, 1, 6, 258, .109]
-        }]
+        });
     });
 });
+
--- app/assets/javascripts/rank-column.js
+++ app/assets/javascripts/rank-column.js
@@ -1,31 +1,33 @@
 $(function () {
-    $('#rank_column').highcharts({
-        chart: {
-            type: 'column',
-            options3d: {
-                enabled: true,
-                alpha: 15,
-                beta: 15,
-                depth: 50,
-                viewDistance: 25
-            }
-        },
-        title: {
-            text: 'Category Leaderboard'
-        },
-        subtitle: {
-            text: 'Category: Total Overall'
-        },
-        plotOptions: {
-            column: {
-                depth: 25
-            }
-        },
-        xAxis: {
-            categories: ['Mark', 'Jacob', 'Larry', 'Bob', 'Derek', 'Chris', 'Daniel', 'Sarah', 'Taylor', 'Mitch', 'Alex', 'Devin']
-        },
-        series: [{
-            data: [1000, 900, 900, 800, 700, 600, 600, 500, 400, 300, 200, 100]
-        }]
+    $('#rank_column').waypoint(function () {
+        $('#rank_column').highcharts({
+            chart: {
+                type: 'column',
+                options3d: {
+                    enabled: true,
+                    alpha: 15,
+                    beta: 15,
+                    depth: 50,
+                    viewDistance: 25
+                }
+            },
+            title: {
+                text: 'Category Leaderboard'
+            },
+            subtitle: {
+                text: 'Category: Total Overall'
+            },
+            plotOptions: {
+                column: {
+                    depth: 25
+                }
+            },
+            xAxis: {
+                categories: ['Mark', 'Jacob', 'Larry', 'Bob', 'Derek', 'Chris', 'Daniel', 'Sarah', 'Taylor', 'Mitch', 'Alex', 'Devin']
+            },
+            series: [{
+                data: [1000, 900, 900, 800, 700, 600, 600, 500, 400, 300, 200, 100]
+            }]
+        });
     });
 });
