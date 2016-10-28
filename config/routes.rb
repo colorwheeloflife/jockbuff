830d30b836f1aca57b8127c74b51d7d7750eacf3
Sarah Veness <sarahhveness@gmail.com>
2016-10-28 23:03:27 +0000

    routes updated for players/compares

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,35 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'welcome#welcome'
+ root to: 'welcome#welcome'
 
-  resources :users do
-    resources :leagues, only: [:index, :show] do
-      resources :teams, only: [:index, :show, :create] do
-        resources :players, only: [:index]
-          resources :compares, only: [:index]
-      end
-    end
-  end
+ resources :users do
+   resources :leagues do
+     resources :teams, only: [:index, :show, :create] do
+     end
+   end
+ end
 
+ resources :analysis, only: [:index, :show]
 
-  resources :analysis, only: [:index, :show]
+ get '/users/:user_id/leagues' => 'leagues#create'
 
-  get '/users/:user_id/leagues' => 'leagues#create'
+ get '/users/:user_id/leagues/:leagueid/players' => 'players#index', :as => :players
+ get '/users/:user_id/leagues/:leagueid/compares' => 'compares#index', :as => :compares
 
-  get '/users/:user_id/leagues/:leagueid/players' => 'players#index', :as => :players
-  get '/users/:user_id/leagues/:leagueid/compares' => 'compares#index', :as => :compares
+ get '/users/:user_id/leagues/:league_id/teams' => 'teams#index'
+ post '/users/:user_id/leagues/:league_id/teams' => 'teams#create'
+ post '/users/:user_id/leagues/:id' => 'teams#create'
 
+ get '/auth/:provider/callback' => 'sessions#create'
 
-  post '/users/:user_id/leagues/:league_id/teams' => 'teams#create'
-  post '/users/:user_id/leagues/:id' => 'teams#create'
+ get '/signin' => 'sessions#new', :as => :signin
 
-  get '/auth/:provider/callback' => 'sessions#create'
+ get '/auth/failure' => 'sessions#failure'
 
-  get '/signin' => 'sessions#new', :as => :signin
-
-  get '/auth/failure' => 'sessions#failure'
-
-  get '/update_players_table' => 'players#create'
+ get '/update_players_table' => 'players#create'
 
 
-  delete '/signout' => 'sessions#destroy'
+ delete '/signout' => 'sessions#destroy'
 
 end
