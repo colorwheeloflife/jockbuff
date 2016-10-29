453264164950f5994996a6d0e6ef93e7a01a4b2a
Chad Bruhaug <churfzilla@gmail.com>
2016-10-29 16:23:20 -0700

    changed names to be more clear

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -1,8 +1,8 @@
 class ComparesController < ApplicationController
 
   def index
-    @current_user_players = GetCurrentUserTeamInfo.new.get_current_user_roster(params[:league_id])
-    @user_team_name = GetCurrentUserTeamInfo.new.get_user_team_name(params[:user_id], params[:league_id])
+    @current_user_players = GetCurrentUserTeamInfo.new.get_roster(params[:league_id])
+    @user_team_name = GetCurrentUserTeamInfo.new.get_team_name(params[:user_id], params[:league_id])
   end
 
 end
--- app/services/get_current_user_team_info.rb
+++ app/services/get_current_user_team_info.rb
@@ -2,13 +2,13 @@ class GetCurrentUserTeamInfo
 
   attr_accessor :league_id, :team_id, :player, :user_id
 
-  def get_current_user_roster(league_id)
+  def get_roster(league_id)
     @players = PlayerPassport.where(league_id: true)
     team = Team.where(league_id: league_id, ownership: true).pluck(:id)
     @user_players = PlayerPassport.where(team_id: team).includes(:player)
   end
 
-  def get_user_team_name(league_id, user_id)
+  def get_team_name(league_id, user_id)
     @team_name = Team.where(league_id: league_id, ownership: true).pluck(:name).join("")
   end
 
