5778882c2de9e1c8e2f473d250883d641bd09608
Chad Bruhaug <churfzilla@gmail.com>
2016-10-29 15:14:42 -0700

    Stylized and initiated instance variables for trades page

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -1,2 +1,14 @@
 class ComparesController < ApplicationController
+  include TeamsHelper
+  include ApplicationHelper
+
+  def index
+    league_id = params[:league_id]
+    @players = PlayerPassport.where(league_id: true)
+    @team_name = Team.where(league_id: league_id, ownership: true).pluck(:name).join("")
+    team = Team.where(league_id: league_id, ownership: true).pluck(:id)
+    @current_user_players = PlayerPassport.where(team_id: team).includes(:player)
+    # @players = PlayerPassport.where(team_id: team_id).includes(:player)
+  end
+
 end
