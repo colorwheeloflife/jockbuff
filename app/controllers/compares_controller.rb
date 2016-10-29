83e0aff08541e1cb23ca20fedc25074ac76215b9
Chad Bruhaug <churfzilla@gmail.com>
2016-10-29 16:21:03 -0700

    Created compares service and are rendering the users players to the page

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -1,14 +1,8 @@
 class ComparesController < ApplicationController
-  include TeamsHelper
-  include ApplicationHelper
 
   def index
-    league_id = params[:league_id]
-    @players = PlayerPassport.where(league_id: true)
-    @team_name = Team.where(league_id: league_id, ownership: true).pluck(:name).join("")
-    team = Team.where(league_id: league_id, ownership: true).pluck(:id)
-    @current_user_players = PlayerPassport.where(team_id: team).includes(:player)
-    # @players = PlayerPassport.where(team_id: team_id).includes(:player)
+    @current_user_players = GetCurrentUserTeamInfo.new.get_current_user_roster(params[:league_id])
+    @user_team_name = GetCurrentUserTeamInfo.new.get_user_team_name(params[:user_id], params[:league_id])
   end
 
 end
