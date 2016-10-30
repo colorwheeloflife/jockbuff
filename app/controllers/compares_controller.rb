20b5a9e8b02432dc9f21467ca6d67c8b6c44b75f
Chad Bruhaug <churfzilla@gmail.com>
2016-10-30 16:17:30 -0700

    Ajax functioning for players to be traded render

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -11,4 +11,9 @@ class ComparesController < ApplicationController
     render :partial => 'render_trade_table'
   end
 
+  def players_to_be_traded
+    @players = PlayerPassport.where(team_id: team_id).includes(:player)
+    render :partial => 'players_to_be_traded'
+  end
+
 end
