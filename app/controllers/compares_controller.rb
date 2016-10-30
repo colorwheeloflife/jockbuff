70510f558021d147ba9268df49673033843fd01e
Chad Bruhaug <churfzilla@gmail.com>
2016-10-29 17:35:27 -0700

    Created a dropdown button for selecting trading partners

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -3,6 +3,7 @@ class ComparesController < ApplicationController
   def index
     @current_user_players = GetCurrentUserTeamInfo.new.get_roster(params[:league_id])
     @user_team_name = GetCurrentUserTeamInfo.new.get_team_name(params[:user_id], params[:league_id])
+    @team_name = Team.where(league_id: params[:league_id], ownership: false).pluck(:name)
   end
 
 end
