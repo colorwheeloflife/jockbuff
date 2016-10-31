8a8912654daa9467ec4d9cfd34f65c1b0cd3b7b1
Taylor Aucoin <colorwheeloflife@gmail.com>
2016-10-31 01:37:09 +0000

    Added optional functionality for all the player + goalie categories to the players index table.

--- app/models/league.rb
+++ app/models/league.rb
@@ -3,6 +3,7 @@ class League < ApplicationRecord
     has_many :teams
     has_many :player_passports
     has_one :player_category
+    has_one :goalie_category
     validates :user_id , presence: true
 
 end
