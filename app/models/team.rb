869c12123bd8d42cc0a48932d34a3e2e008dbb6c
Sarah Veness <sarahhveness@gmail.com>
2016-11-01 03:23:54 +0000

    minor cleanup of spacing

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
-    belongs_to :league
-    has_many :players, :through => :player_passports
-    has_many :player_passports
-    validates :name, uniqueness: {scope: :league}
+  belongs_to :league
+  has_many :players, :through => :player_passports
+  has_many :player_passports
+  validates :name, uniqueness: {scope: :league}
 end
