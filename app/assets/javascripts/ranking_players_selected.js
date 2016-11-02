0f56ae288381b9e89ebce8ef43ed824bcbf3785c
Taylor Aucoin <colorwheeloflife@gmail.com>
2016-11-02 02:27:10 +0000

    Little tweaks before ready to pull.

--- app/assets/javascripts/ranking_players_selected.js
+++ app/assets/javascripts/ranking_players_selected.js
@@ -16,25 +16,6 @@ $(function() {
     return array[index];
   }
 
-
-
-  // $('#playersTable').on('check.bs.table', function (e, row) {
-  //   selectedPlayers.push(row);
-  // });
-
-
-
-  // $('#playersTable').on('uncheck.bs.table', function (e, row) {
-  //       console.log(row);
-  //       var index = $(row).prop('data-index');
-  //       console.log(index);
-  //       var index_num = parseInt(index);
-  //       console.log(index_num);
-  //       selectedPlayers.splice(index_num, 1);
-  // });
-
-
-
   $("#add_player").click(function() {
     $("#selection-output").empty();
 
@@ -48,25 +29,25 @@ $(function() {
       row.append($('<td>').text(value.pro_team));
       row.append($('<td>').text(value.positions));
       row.append($('<td>').text(value.jbr));
-      if (value.g != undefined) { row.append($('<td>').text(value.g)); }
-      if (value.a != undefined) { row.append($('<td>').text(value.a)); }
-      if (value.p != undefined) { row.append($('<td>').text(value.p)); }
-      if (value.ppg != undefined) { row.append($('<td>').text(value.ppg)); }
-      if (value.ppa != undefined) { row.append($('<td>').text(value.ppa)); }
-      if (value.ppp != undefined) { row.append($('<td>').text(value.ppp)); }
-      if (value.plusminus != undefined) { row.append($('<td>').text(value.plusminus)); }
-      if (value.shg != undefined) { row.append($('<td>').text(value.shg)); }
-      if (value.sha != undefined) { row.append($('<td>').text(value.sha)); }
-      if (value.shp != undefined) { row.append($('<td>').text(value.shp)); }
-      if (value.gwg != undefined) { row.append($('<td>').text(value.gwg)); }
-      if (value.gtg != undefined) { row.append($('<td>').text(value.gtg)); }
-      if (value.pim != undefined) { row.append($('<td>').text(value.pim)); }
-      if (value.sog != undefined) { row.append($('<td>').text(value.sog)); }
-      if (value.shpercent != undefined) { row.append($('<td>').text(value.shpercent)); }
-      if (value.fw != undefined) { row.append($('<td>').text(value.fw)); }
-      if (value.fl != undefined) { row.append($('<td>').text(value.fl)); }
-      if (value.hit != undefined) { row.append($('<td>').text(value.hit)); }
-      if (value.blk != undefined) { row.append($('<td>').text(value.blk)); }
+      if (value.g !== undefined) { row.append($('<td>').text(value.g)); }
+      if (value.a !== undefined) { row.append($('<td>').text(value.a)); }
+      if (value.p !== undefined) { row.append($('<td>').text(value.p)); }
+      if (value.ppg !== undefined) { row.append($('<td>').text(value.ppg)); }
+      if (value.ppa !== undefined) { row.append($('<td>').text(value.ppa)); }
+      if (value.ppp !== undefined) { row.append($('<td>').text(value.ppp)); }
+      if (value.plusminus !== undefined) { row.append($('<td>').text(value.plusminus)); }
+      if (value.shg !== undefined) { row.append($('<td>').text(value.shg)); }
+      if (value.sha !== undefined) { row.append($('<td>').text(value.sha)); }
+      if (value.shp !== undefined) { row.append($('<td>').text(value.shp)); }
+      if (value.gwg !== undefined) { row.append($('<td>').text(value.gwg)); }
+      if (value.gtg !== undefined) { row.append($('<td>').text(value.gtg)); }
+      if (value.pim !== undefined) { row.append($('<td>').text(value.pim)); }
+      if (value.sog !== undefined) { row.append($('<td>').text(value.sog)); }
+      if (value.shpercent !== undefined) { row.append($('<td>').text(value.shpercent)); }
+      if (value.fw !== undefined) { row.append($('<td>').text(value.fw)); }
+      if (value.fl !== undefined) { row.append($('<td>').text(value.fl)); }
+      if (value.hit !== undefined) { row.append($('<td>').text(value.hit)); }
+      if (value.blk !== undefined) { row.append($('<td>').text(value.blk)); }
     });
     graph_data = [];
     $.each(selectedPlayers, function(index, value) {
@@ -76,25 +57,25 @@ $(function() {
         data: data
       };
       data.push(parseInt(value.jbr));
-      if (value.g != undefined) { data.push(parseInt(value.g)); }
-      if (value.a != undefined) { data.push(parseInt(value.a)); }
-      if (value.p != undefined) { data.push(parseInt(value.p)); }
-      if (value.ppg != undefined) { data.push(parseInt(value.ppg)); }
-      if (value.ppa != undefined) { data.push(parseInt(value.ppa)); }
-      if (value.ppp != undefined) { data.push(parseInt(value.ppp)); }
-      if (value.plusminus != undefined) { data.push(parseInt(value.plusminus)); }
-      if (value.shg != undefined) { data.push(parseInt(value.shg)); }
-      if (value.sha != undefined) { data.push(parseInt(value.sha)); }
-      if (value.shp != undefined) { data.push(parseInt(value.shp)); }
-      if (value.gwg != undefined) { data.push(parseInt(value.gwg)); }
-      if (value.gtg != undefined) { data.push(parseInt(value.gtg)); }
-      if (value.pim != undefined) { data.push(parseInt(value.pim)); }
-      if (value.sog != undefined) { data.push(parseInt(value.sog)); }
-      if (value.shpercent != undefined) { data.push(parseInt(value.shpercent)); }
-      if (value.fw != undefined) { data.push(parseInt(value.fw)); }
-      if (value.fl != undefined) { data.push(parseInt(value.fl)); }
-      if (value.hit != undefined) { data.push(parseInt(value.hit)); }
-      if (value.blk != undefined) { data.push(parseInt(value.blk)); }
+      if (value.g !== undefined) { data.push(parseInt(value.g)); }
+      if (value.a !== undefined) { data.push(parseInt(value.a)); }
+      if (value.p !== undefined) { data.push(parseInt(value.p)); }
+      if (value.ppg !== undefined) { data.push(parseInt(value.ppg)); }
+      if (value.ppa !== undefined) { data.push(parseInt(value.ppa)); }
+      if (value.ppp !== undefined) { data.push(parseInt(value.ppp)); }
+      if (value.plusminus !== undefined) { data.push(parseInt(value.plusminus)); }
+      if (value.shg !== undefined) { data.push(parseInt(value.shg)); }
+      if (value.sha !== undefined) { data.push(parseInt(value.sha)); }
+      if (value.shp !== undefined) { data.push(parseInt(value.shp)); }
+      if (value.gwg !== undefined) { data.push(parseInt(value.gwg)); }
+      if (value.gtg !== undefined) { data.push(parseInt(value.gtg)); }
+      if (value.pim !== undefined) { data.push(parseInt(value.pim)); }
+      if (value.sog !== undefined) { data.push(parseInt(value.sog)); }
+      if (value.shpercent !== undefined) { data.push(parseInt(value.shpercent)); }
+      if (value.fw !== undefined) { data.push(parseInt(value.fw)); }
+      if (value.fl !== undefined) { data.push(parseInt(value.fl)); }
+      if (value.hit !== undefined) { data.push(parseInt(value.hit)); }
+      if (value.blk !== undefined) { data.push(parseInt(value.blk)); }
       graph_data.push(player_data);
     });
     linegraph();
