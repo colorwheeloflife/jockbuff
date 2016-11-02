0aa577f1618009d23d01e41a474357c9e0d93c6f
Sarah Veness <sarahhveness@gmail.com>
2016-11-02 23:10:53 +0000

    nav bar updated with teams

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   def welcome
      auth = request.env["omniauth.auth"]
     # puts "\n\n\n\nAUTH: #{auth}\n\n\n"
+    @leagues = League.where(user_id: current_user.id)
+    @team_id = Team.where(ownership: true).pluck(:id)
   end
 end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,4 +1,3 @@
 module PlayersHelper
 
 end
-
