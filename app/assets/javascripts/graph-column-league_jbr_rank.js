801935620c8494cca4456bc357738728113251bf
Sarah Veness <sarahhveness@gmail.com>
2016-11-03 20:30:36 +0000

    update to JBR

--- app/assets/javascripts/graph-column-league_jbr_rank.js
+++ app/assets/javascripts/graph-column-league_jbr_rank.js
@@ -6,24 +6,24 @@ $(function() {
     var JbrLeagueRankData = getLeagueChartData();
 
     for (var i = 0; i < JbrLeagueRankData.length; i++) {
-          var obj = JbrLeagueRankData[i];
-          var team_name = obj[Object.keys(obj)[0]];
-          var team_jbr = obj[Object.keys(obj)[1]];
-          leagueTeams.push(team_name);
-          leagueJBRs.push(team_jbr);
-      }
+      var obj = JbrLeagueRankData[i];
+      var team_name = obj[Object.keys(obj)[0]];
+      var team_jbr = obj[Object.keys(obj)[1]];
+      leagueTeams.push(team_name);
+      leagueJBRs.push(team_jbr);
+    }
 
     $('#rank_column-top').highcharts({
-          chart: {
-              type: 'column',
-              options3d: {
-                  enabled: true,
-                  alpha: 15,
-                  beta: 15,
-                  depth: 50,
-                  viewDistance: 25
-              }
-          },
+        chart: {
+          type: 'column',
+          options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50,
+            viewDistance: 25
+          }
+        },
           title: {
               text: 'Category Leaderboard'
           },
